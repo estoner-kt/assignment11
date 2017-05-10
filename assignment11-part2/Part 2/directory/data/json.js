efdd93b8f3a713817cb538f6c7af5982afb67e27
Kate Stoner <estoner.kt@gmail.com>
2017-05-09 17:47:34 -0700

    g
    g

---
+++ assignment11-part2/Part 2/directory/data/json.js
@@ -0,0 +1,100 @@
+{"teammembers":[
+	{
+		"name":"Amy Jones",
+		"title":"VP of Accounting",
+		"ImagePath": "images/amy_jones.jpg",
+        "OfficeNumber": "619-212-1592", 
+        "CellNumber":  "619-212-1592", 
+        "Email": "amy@gmail.com"
+
+	},
+	{
+		"name":"Eugene Lee",
+		"title":"Director of Development",
+     "ImagePath":"images/eugene_lee.jpg", 
+        "OfficeNumber": "619-212-1500", 
+        "CellNumber":  "619-212-1500", 
+        "Email": "eugene@gmail.com"
+		
+	},
+	{
+		"name":"Gary Donovan",
+		"title":"CFO",
+		"ImagePath": "images/gary_donovan.jpg",
+        "OfficeNumber": "619-212-0000", 
+        "CellNumber":  "619-212-0000", 
+        "Email": "gary@gmail.com"
+	},
+	{
+		"name":"James King",
+		"title":"Intern",
+		"ImagePath": "images/james_king.jpg",
+        "OfficeNumber": "619-212-0001", 
+        "CellNumber":  "619-212-0001", 
+        "Email": "james@gmail.com"
+	},
+	{
+		"name":"John Williams",
+		"title":"Web Developer",
+		"ImagePath": "images/john_williams.jpg",
+        "OfficeNumber": "619-212-0002", 
+        "CellNumber":  "619-212-0002", 
+        "Email": "john@gmail.com"
+	}, 
+    {
+		"name":"Julie Talor",
+		"title":"Web Developer",
+		"ImagePath": "images/julie_taylor.jpg",
+        "OfficeNumber": "619-212-0003", 
+        "CellNumber":  "619-212-0003", 
+        "Email": "julie@gmail.com"
+	}, 
+    {
+		"name":"Lisa Wong",
+		"title":"Web Developer",
+		"ImagePath": "images/lisa_wong.jpg",
+        "OfficeNumber": "619-212-0004", 
+        "CellNumber":  "619-212-0004", 
+        "Email": "lisa@gmail.com"
+	}, 
+    {
+		"name":"Kathleen Byrne",
+		"title":"Web Developer",
+		"ImagePath": "images/kathleen_byrne.jpg",
+        "OfficeNumber": "619-212-0005", 
+        "CellNumber":  "619-212-0005", 
+        "Email": "kathleen@gmail.com"
+	}, 
+    {
+		"name":"Paul Jones",
+		"title":"Cleaner",
+		"ImagePath": "images/paul_jones.jpg",
+        "OfficeNumber": "619-212-0006", 
+        "CellNumber":  "619-212-0006", 
+        "Email": "paul@gmail.com"
+	},
+    {
+		"name":"Paula Gates",
+		"title":"Cook",
+		"ImagePath": "images/paula_gates.jpg",
+        "OfficeNumber": "619-212-0007", 
+        "CellNumber":  "619-212-0007", 
+        "Email": "paula@gmail.com"
+	}, 
+    {
+		"name":"Ray Moore",
+		"title":"Designer",
+		"ImagePath": "images/ray_moore.jpg",
+        "OfficeNumber": "619-212-0008", 
+        "CellNumber":  "619-212-0008", 
+        "Email": "ray@gmail.com"
+	}, 
+    {
+		"name":"Steven Wells",
+		"title":"Cook",
+		"ImagePath": "images/steven_wells.jpg",
+        "OfficeNumber": "619-212-0008", 
+        "CellNumber":  "619-212-0008", 
+        "Email": "steven@gmail.com"
+	}
+]}
